56e5585a8deee338a9257fad7eb34fb11214f30f
Yoonseo Park <58269015+yoonseopark001@users.noreply.github.com>
2019-12-30 03:30:13 +0900

    Update k-meansClustering-with-scikit-learn.py

--- k-meansClustering-with-scikit-learn.py
+++ k-meansClustering-with-scikit-learn.py
@@ -102,5 +102,27 @@ model = KMeans(n_clusters=3)
 model.fit(samples)
 print(model.inertia_)       # after fit(), available as attribute inertia_
 
+##############################################################################
+
+# https://archive.ics.uci.edu/ml/datasets/seeds
+ks = range(1, 6)
+inertias = []
+
+for k in ks:
+    # Create a KMeans instance with k clusters: model
+    model = KMeans(n_clusters=k)
+
+    # Fit model to samples
+    model.fit(samples)
+    
+    # Append the inertia to the list of inertias
+    inertias.append(model.inertia_)
+    
+# Plot ks vs inertias
+plt.plot(ks, inertias, '-o')
+plt.xlabel('number of clusters, k')
+plt.ylabel('inertia')
+plt.xticks(ks)
+plt.show()
 
 
